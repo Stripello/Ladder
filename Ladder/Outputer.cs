550853f010b028dc13f30fa0495816de1b09d1d9
Владлен Шапарев <shve@outlook.com>
2022-03-19 13:33:26 +0700

    Add project files.

---
+++ Ladder/Outputer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ladder
+{
+    class Outputer
+    {
+        internal static void Output(uint input)
+        {
+            
+            for (uint i = 1; i <= input; i++)
+            {
+                for (uint j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+
+            Console.Write('\n');
+            if (input % 2 == 0) { input++; }
+
+            for (uint i = 0; i<= input/2; i++)
+            {
+                for (uint j = 0; j < input/2-i; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (uint j = 1; j <= i*2+1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
---
+++ Ladder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ladder
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter number to create \"сhristmass tree\" or type \"Exit\" to terminate process.");
+                string input = Console.ReadLine();
+                if (input.ToLower().Equals("exit")) return;
+                bool succedParsing = uint.TryParse(input, out uint inputNumber);
+                if (succedParsing) { Outputer.Output(inputNumber); }
+                else { Console.WriteLine("Can't parse input string."); }
+            }
+        }
+    }
+}
